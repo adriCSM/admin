bfcafe0fa0ac606375586bbcebcba282b76eee44
Adri Candra Saputra Mangidi <adricandrasaputramangidi@gmail.com>
2023-07-21 20:00:42 +0800

    add message crud

--- src/validators/Portofolio/Contact/schema.js
+++ src/validators/Portofolio/Contact/schema.js
@@ -2,9 +2,9 @@ import Joi from 'joi';
 
 const postContactPayloadSchema = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string().required(),
-  mobileNumber: Joi.string().required(),
-  message: Joi.string().required(),
+  email: Joi.string().required().email({ tlds: true }),
+  mobileNumber: Joi.string().required().min(11).max(12),
+  message: Joi.string().required().max(500),
 });
 
 export default postContactPayloadSchema;
