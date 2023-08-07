8815e5ab6fb46fa8651bd0ca72c9382ddc5792a8
Adri Candra Saputra Mangidi <adricandrasaputramangidi@gmail.com>
2023-08-07 16:54:45 +0800

    feat: update error message

--- src/validators/User/schema.js
+++ src/validators/User/schema.js
@@ -1,16 +1,22 @@
 import Joi from 'joi';
 
+const message = {
+  'string.empty': '{#label} is required',
+};
+
 const UserPayloadSchema = Joi.object({
-  username: Joi.string().required(),
-  phoneNumber: Joi.string().length(12).required(),
+  username: Joi.string().required().messages(message),
+  phoneNumber: Joi.string().length(12).required().messages(message),
   email: Joi.string()
     .email({ tlds: { allow: ['com', 'net', 'id', 'ac', 'uho'] } })
-    .required(),
+    .required()
+    .messages(message),
   password: Joi.string()
     .min(8)
     .regex(/^(?=.*[A-Z])(?=.*[!@#$%^&*])/)
-    .required(),
-  confirmPassword: Joi.string().required(),
+    .required()
+    .messages(message),
+  confirmPassword: Joi.string().required().messages(message),
 });
 
 const putUserPayloadSchema = Joi.object({
