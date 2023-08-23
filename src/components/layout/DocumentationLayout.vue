05bd55229031a6c9fbc4902975bfe47bd3299d9b
Adri Candra Saputra Mangidi <adricandrasaputramangidi@gmail.com>
2023-08-23 11:28:13 +0800

    feat: ADD envkey

--- src/components/layout/DocumentationLayout.vue
+++ src/components/layout/DocumentationLayout.vue
@@ -227,6 +227,69 @@ const mode = computed(() => {
           </v-card>
         </section>
 
+        <section id="putUser">
+          <h5 class="text-blue font-weight-bold">PUT User</h5>
+          <h6>Endpoint</h6>
+          <v-card width="100%" height="auto" class="my-5" :class="mode">
+            <v-card-title class="text-body-1"> PUT /users </v-card-title>
+          </v-card>
+          <h6>Headers</h6>
+          <v-card class="my-5" :class="mode">
+            <BtnCopy class-name="getUsersHeaders" />
+
+            <pre class="getUsersHeaders">
+              <code >
+      { 
+        Headers: { 
+          Authorization: Bearer &lt;accessToken&gt; 
+          Accept: ['application/vnd.api+json', 'multipart/form-data'],
+        } 
+      }
+              </code>
+            </pre>
+          </v-card>
+          <h6>Payload</h6>
+          <v-card class="my-5" :class="mode">
+            <BtnCopy class-name="getUsersHeaders" />
+
+            <pre class="getUsersHeaders">
+              <code >
+      { 
+        Headers: { 
+          Authorization: Bearer &lt;accessToken&gt; 
+        } 
+      }
+              </code>
+            </pre>
+          </v-card>
+          <h6>Response</h6>
+          <v-card class="my-5" :class="mode">
+            <BtnCopy class-name="getUsersResponse" />
+            <pre class="getUsersResponse">
+              <code>
+      {
+        "status":"success",
+        "data": [
+          {
+            "_id": "64a80dd2082ef6f61e879677",
+            "username": "adri",
+            "phone_number": "081234567890",
+            "email": "adri@gmail.com",
+            "role": "adri",
+            "pic": "https://img.icons8.com/ios-filled/50/000000/user-male-circle.png",
+            "createdAt": "2023-07-07T13:06:26.860Z",
+            "updatedAt": "2023-08-03T15:40:00.052Z",
+            "__v": 0,
+            "isOnline": true
+          },
+          ...
+        ]
+      }
+              </code>
+            </pre>
+          </v-card>
+        </section>
+
         <v-divider color="blue" class="opacity-100" thickness="3"></v-divider>
       </v-col>
     </v-row>
