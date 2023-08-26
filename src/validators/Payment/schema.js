efc235c2ec0bf6c54c184d0800ab2594b44b663b
Adri Candra Saputra Mangidi <adricandrasaputramangidi@gmail.com>
2023-08-26 21:44:59 +0800

    feat: update payment validator message

--- src/validators/Payment/schema.js
+++ src/validators/Payment/schema.js
@@ -1,10 +1,17 @@
 import Joi from 'joi';
 
+const messages = {
+  'string.empty': '#{label} is required',
+};
+
 const paymentPayloadSchema = Joi.object({
-  firstName: Joi.string().required(),
-  lastName: Joi.string().required(),
-  email: Joi.string().required(),
-  grossAmount: Joi.number().required().options({ convert: false }),
+  firstName: Joi.string().required().message(messages),
+  lastName: Joi.string().required().message(messages),
+  email: Joi.string()
+    .email({ tlds: { allow: ['com', 'net', 'id', 'ac', 'uho'] } })
+    .required()
+    .message(messages),
+  grossAmount: Joi.number().required().options({ convert: false }).message(messages),
 });
 
 export default paymentPayloadSchema;
