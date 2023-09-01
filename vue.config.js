5791630e714e4b4051352ec59f11ef887aa1af05
Adri Candra Saputra Mangidi <adricandrasaputramangidi@gmail.com>
2023-09-01 21:24:59 +0800

    feat:add pwa

--- vue.config.js
+++ vue.config.js
@@ -8,4 +8,21 @@ module.exports = defineConfig({
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
     },
   },
+
+  pwa: {
+    name: 'Web AM',
+    themeColor: '#6368D9',
+    msTileColor: '#6368D9',
+    manifestOptions: {
+      background_color: '#6368D9',
+    },
+    iconPaths: {
+      faviconSVG: 'img/icons/LOGO.png',
+      favicon32: 'img/icons/LOGO.png',
+      favicon16: 'img/icons/LOGO.png',
+      appleTouchIcon: 'img/icons/LOGO.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png',
+    },
+  },
 });
