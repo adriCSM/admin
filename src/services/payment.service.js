5a15c1a9a3aa7fcc632bee02789df97829c1eea9
Adri Candra Saputra Mangidi <adricandrasaputramangidi@gmail.com>
2023-09-02 13:47:49 +0800

    feat:update headers

--- src/services/payment.service.js
+++ src/services/payment.service.js
@@ -11,6 +11,9 @@ export default {
         headers: {
           'Content-Type': 'application/json',
           Accept: 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Headers': 'Content-Type',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
         },
       },
     );
